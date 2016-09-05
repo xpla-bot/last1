b2e90c0442c8eadd08a04db06714917bd50f0f37
Mikołaj Leśny <MakaszML@gmail.com>
2016-09-05 11:46:24 +0200

    Display result

--- game3.js
+++ game3.js
@@ -1,16 +1,24 @@
-var curTable, amount, currency, fcurrency;
+var curTable;
 
 $.get('https://openexchangerates.org/api/latest.json', {app_id: 'b54fc8736f7142118122fddb2c9dc837'}, function(data){
   curTable = JSON.stringify(data);
 });
 
-//9/Get user data from input fields using class selectors and refresh on keystroke
+
+
 $(".currencyIn").keyup(function(){
   amount = ($(this).val());
 });
 $(".currency").keyup(function(){
   currency = ($(this).val());
+  console.log(amount + currency);
 });
 $(".currencyIn3").keyup(function(){
   fcurrency = ($(this).val());
 });
+
+//3/ Operate on data from API and user to return wanted result. Bind function to proper filed and refresh on click.
+$(".currencyOut").click(function(){
+  //1/Reformat strng to JSON and select desired fields
+  $(".currencyOutP").text((JSON.parse(curTable).rates[currency]*amount/(JSON.parse(curTable).rates[fcurrency])*amount).toFixed(2));
+})
