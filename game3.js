a4e04d4a6329952b86dded21a3a4817071f7444b
Mikołaj Leśny <MakaszML@gmail.com>
2016-09-05 11:46:02 +0200

    Get user data

--- game3.js
+++ game3.js
@@ -1,6 +1,16 @@
-var curTable;
-//3/Use jQuery method .get to request data from API
+var curTable, amount, currency, fcurrency;
+
 $.get('https://openexchangerates.org/api/latest.json', {app_id: 'b54fc8736f7142118122fddb2c9dc837'}, function(data){
-  //1/Write JSON response as string to variable for later use
   curTable = JSON.stringify(data);
+});
+
+//9/Get user data from input fields using class selectors and refresh on keystroke
+$(".currencyIn").keyup(function(){
+  amount = ($(this).val());
+});
+$(".currency").keyup(function(){
+  currency = ($(this).val());
+});
+$(".currencyIn3").keyup(function(){
+  fcurrency = ($(this).val());
 });
